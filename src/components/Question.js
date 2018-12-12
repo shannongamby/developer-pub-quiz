b4757cd758a66806550088039101f849d5b31127
NaomiBid <naomi0bid@gmail.com>
2018-12-12 15:55:11 +0000

    added alert message for no button

--- src/components/Question.js
+++ src/components/Question.js
@@ -6,12 +6,16 @@ class Question extends Component {
     alert('No, you numpty!')
   }
 
+  correct() {
+    alert("You're acing this!")
+  }
+
   render() {
     return (
       <div className="App">
         <div>Is 1 greater than 2?</div>
         <button id='yes_button' onClick={this.incorrect}>YES</button>
-        <button id='no_button'>NO</button>
+        <button id='no_button' onClick={this.correct}>NO</button>
       </div>
     );
   }
