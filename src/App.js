faa4d71434e7f913bfb6601778521dcc7dcef477
Rupert Adams <RupertNCAdams@aol.com>
2018-12-13 11:59:21 +0000

    tested and passed App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Question from './components/Question';
-import QuestionOne from './components/QuestionOne';
-import QuizQuestions from './quizquestions/QuizQuestions';
 
 class App extends Component {
   render() {
     return (
-      <div className="Question">
-        < Question />
-        < QuestionOne />
-        < QuizQuestions />
+      <div>
       </div>
     );
   }
--- src/components_tests/App.test.js
+++ src/components_tests/App.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../App.js';
-import QuestionOne from '../components/QuestionOne';
 import { shallow, mount, render } from 'enzyme';
 
 describe('<App />', () => {
@@ -9,10 +8,5 @@ describe('<App />', () => {
     const div = document.createElement('div');
     ReactDOM.render(<App />, div);
     ReactDOM.unmountComponentAtNode(div);
-  });
-
-  it('renders <QuestionOne />', () => {
-    const wrapper = shallow(<QuestionOne />);
-    expect(wrapper.text()).toEqual('Render the question');
   });
 });
