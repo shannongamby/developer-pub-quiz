3deb595a0e99a819e65b7fa8c8172ec78cce1da6
Edyta Bednarz <edy.b.bednarz@gmail.com>
2018-12-13 15:23:28 +0000

    Renamed file for clarity.

---
+++ src/quizquestions/data.js
@@ -0,0 +1,68 @@
+let quizData = [
+ {
+   question: {
+     text: "Is the earth flat?"
+   },
+   options: [
+     {
+       text: "Yes!!!!!!!!!!!",
+       isCorrect: true
+     },
+     {
+       text: "No",
+       isCorrect: false
+     }
+   ]
+ },
+ {
+   question: {
+     text: "Is 1 greater than 2?"
+   },
+   options: [
+     {
+       text: "Yes",
+       isCorrect: false
+     },
+     {
+       text: "No",
+       isCorrect: true
+     }
+   ]
+ },
+ {
+   question: {
+     text: "What's the meaning of life?"
+   },
+   options: [
+     {
+       text: "42",
+       isCorrect: true
+     },
+     {
+       text: "147",
+       isCorrect: false
+     },
+     {
+       text: "Duck",
+       isCorrect: false
+     }
+   ]
+ },
+ {
+   question: {
+     text: "Is Brazil in North America?"
+   },
+   options: [
+     {
+       text: "Yes",
+       isCorrect: false
+     },
+     {
+       text: "No",
+       isCorrect: true
+     }
+   ]
+ }
+]
+
+export default quizData;
