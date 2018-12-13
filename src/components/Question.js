7e7659d9d2e9bd5dd281ca4324c141fa25c00bb7
Edyta Bednarz <edy.b.bednarz@gmail.com>
2018-12-13 17:07:47 +0000

    Fixed the invisible token.

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,25 +1,24 @@
-Untitled
 import React, { Component } from 'react';
-​
+
 class Question extends Component {
-​
- constructor(props) {
-  super(props);
-​
-  this.state = {
-   text: props.text,
-  };
- }
-​
- componentWillReceiveProps(nextProps) {
-  this.setState({ text: nextProps.text });
- }
-​
- render() {
-  return (
-   <div>{this.state.text}</div>
-  );
- }
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      text: props.text
+    };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.setState({ text: nextProps.text});
+  }
+
+  render(){
+    return(
+      <div>{ this.state.text }</div>
+    );
+  }
 }
-​
-export default Question; 
+
+export default Question;
