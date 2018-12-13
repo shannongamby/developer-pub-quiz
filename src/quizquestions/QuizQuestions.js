dc01ed1a0acaf64e2529150fc7f15b0b21aef932
Rupert Adams <RupertNCAdams@aol.com>
2018-12-13 12:00:26 +0000

    added file with question and answers

--- src/quizquestions/QuizQuestions.js
+++ src/quizquestions/QuizQuestions.js
@@ -1,9 +1,68 @@
-import React, { Component } from 'react';
+let quizData = [
+ {
+   question: {
+     text: "Is the earth flat?"
+   },
+   options: [
+     {
+       text: "Yes!!!!!!!!!!!",
+       isCorrect: true
+     },
+     {
+       text: "No",
+       isCorrect: false
+     }
+   ]
+ },
+ {
+   question: {
+     text: "Is 1 greater than 2?"
+   },
+   options: [
+     {
+       text: "Yes",
+       isCorrect: false
+     },
+     {
+       text: "No",
+       isCorrect: true
+     }
+   ]
+ },
+ {
+   question: {
+     text: "What's the meaning of life?"
+   },
+   options: [
+     {
+       text: "42",
+       isCorrect: true
+     },
+     {
+       text: "147",
+       isCorrect: false
+     },
+     {
+       text: "Duck",
+       isCorrect: false
+     }
+   ]
+ },
+ {
+   question: {
+     text: "Is Brazil in North America?"
+   },
+   options: [
+     {
+       text: "Yes",
+       isCorrect: false
+     },
+     {
+       text: "No",
+       isCorrect: true
+     }
+   ]
+ }
+]
 
-class QuizQuestions extends Component {
-  render() {
-    return 'Hello'
-  }
-}
-
-export default QuizQuestions;
+export default quizData;
