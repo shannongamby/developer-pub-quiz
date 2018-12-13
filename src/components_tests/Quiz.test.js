b674b768c288c90ebe43a8a4ac4cc1378df82f8c
Rupert Adams <RupertNCAdams@aol.com>
2018-12-13 12:48:57 +0000

    created Quiz class and tests

---
+++ src/components_tests/Quiz.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { shallow, mount, render } from 'enzyme';
+import Quiz from '../components/Quiz';
+
+describe('<Quiz />', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Quiz />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+});
