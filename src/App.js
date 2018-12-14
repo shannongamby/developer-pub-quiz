147ab7dcb29e41db41a42597ffa3f859b2f8484e
NaomiBid <naomi0bid@gmail.com>
2018-12-14 16:23:14 +0000

    added google map to our page

--- src/App.js
+++ src/App.js
@@ -3,15 +3,29 @@ import './App.css';
 import Quiz from './components/Quiz.js'
 import quizData from './quizquestions/data'
 import { Button } from 'react-bootstrap';
+import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Quiz data={ quizData } />
+        <Map google={this.props.google} zoom={14}>
+
+        <Marker onClick={this.onMarkerClick}
+          name={'Current location'} />
+
+          <InfoWindow onClose={this.onInfoWindowClose}>
+          <div>
+        
+          </div>
+          </InfoWindow>
+          </Map>
       </div>
     );
   }
 }
 
-export default App;
+export default GoogleApiWrapper({
+  apiKey: ("AIzaSyCpIBIq2pNTtVdgaWZdqUvRqOqlibzoF4g")
+})(App)
