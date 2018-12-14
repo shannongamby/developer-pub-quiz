9afbf370bc362807a58654330b23f0202bd9b6eb
Edyta Bednarz <edy.b.bednarz@gmail.com>
2018-12-14 12:41:22 +0000

    QuizTwo logic implemented.

---
+++ src/components/QuizTwo.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import Question from '../components/Question'
+import Option from '../components/Option'
+
+class QuizTwo extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      quiz: props.data,
+      currentIndex: 0,
+      score: 0
+    }
+  }
+  handleAnswerSelection(option) {
+    return () => {
+      let newScore = this.state.score + (option.isCorrect ? 1 : 0)
+      let newIndex = this.state.currentIndex + 1
+      let numberOfQuestions = this.state.quiz.length
+      let hasReachedTheEnd = newIndex >= numberOfQuestions
+
+      alert((option.isCorrect ? 'Nice!' : 'Wrong!') + ' Score: ' + newScore)
+
+      if(hasReachedTheEnd) {
+        alert('You reached the end. Your Score: ' + newScore + '/' + numberOfQuestions)
+        this.setState({ score: 0, currentIndex: 0 })
+      } else {
+        this.setState({ score: newScore, currentIndex: newIndex })
+      }
+    }
+  }
+  render(){
+    let currentQuizTwo = this.state.quiz[this.state.currentIndex]
+
+    return(
+      <div><Question text={currentQuizTwo.question.text } />
+      { currentQuizTwo.options.map(this.renderOption.bind(this)) }
+
+      </div>
+    );
+  }
+
+  renderOption(option, index) {
+    return( <Option key={index} text={ option.text } onAnswerSelected={this.handleAnswerSelection(option).bind(this)} />
+  );
+}
+
+}
+
+export default QuizTwo;
---
+++ src/components_tests/QuizTwo.test.js
@@ -0,0 +1,95 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import QuizTwo from '../components/QuizTwo';
+import { shallow, mount, render } from 'enzyme';
+
+let testData = [
+  {
+    question: {
+      text: "Question 1",
+    },
+    options: [
+      {
+        text: "Question 1 - Option 1",
+        isCorrent: true
+      },
+      {
+        text: "Question 1 - Option 2",
+        isCorrent: false
+      },
+      {
+        text: "Question 1 - Option 3",
+        isCorrent: false
+      }
+    ]
+  },{
+    question: {
+      text: "Question 2",
+    },
+    options: [
+      {
+        text: "Question 2 - Option 1",
+        isCorrent: false
+      },
+      {
+        text: "Question 2 - Option 2",
+        isCorrent: true
+      }
+    ]
+  }
+]
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<QuizTwo data={testData} />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders the first question', () => {
+  const wrap = shallow(<QuizTwo data={testData} />)
+
+  expect(wrap.find('Question').prop('text')).toEqual('Question 1')
+
+  expect(wrap.find('Option').length).toEqual(3);
+  expect(wrap.find('Option').at(0).prop('text')).toEqual('Question 1 - Option 1')
+  expect(wrap.find('Option').at(1).prop('text')).toEqual('Question 1 - Option 2')
+  expect(wrap.find('Option').at(2).prop('text')).toEqual('Question 1 - Option 3')
+});
+
+// it('display correct message for correct answer', () => {
+//   var receivedMessage = ""
+//   window.alert = (message) => {
+//     receivedMessage = message // capture the alert message
+//   }
+//   const wrap = shallow(<Quiz data={testData} />)
+//
+//   let correctOptionIndex = 0
+//   wrap.find('Option').at(correctOptionIndex).prop('onAnswerSelected')()
+//   console.log(<Quiz data={testData} />)
+//   expect(receivedMessage).toEqual('Nice! Score: 1')
+// });
+//
+// it('display wrong message for wrong answer', () => {
+//   var receivedMessage = ""
+//   window.alert = (message) => {
+//     receivedMessage = message // capture the alert message
+//   }
+//
+//   const wrap = shallow(<Quiz data={testData} />)
+//   let wrongOptionIndex = 1
+//   wrap.find('Option').at(wrongOptionIndex).prop('onAnswerSelected')()
+//   expect(receivedMessage).toEqual('Wrong! Score: 0')
+// });
+
+it('renders the second question after replying to the first question', () => {
+  window.alert = jest.fn(); // ignore alerts since we're not running the tests in a real browser
+
+  const wrap = shallow(<QuizTwo data={testData} />)
+
+  wrap.find('Option').at(0).prop('onAnswerSelected')()
+
+  expect(wrap.find('Question').prop('text')).toEqual('Question 2')
+  expect(wrap.find('Option').length).toEqual(2);
+  expect(wrap.find('Option').at(0).prop('text')).toEqual('Question 2 - Option 1')
+  expect(wrap.find('Option').at(1).prop('text')).toEqual('Question 2 - Option 2')
+});
