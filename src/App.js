ad8ccbdb72dc5d22652fa437e7605ae21c0f1643
Rupert Adams <RupertNCAdams@aol.com>
2018-12-16 14:09:20 +0000

    styling and resizing the map

--- src/App.js
+++ src/App.js
@@ -8,19 +8,24 @@ import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Quiz data={ quizData } />
-        <Map google={this.props.google} zoom={14}>
+      <div>
+        <div className="App">
+        <style>
+          @import url('https://fonts.googleapis.com/css?family=Cinzel:400,700|Cinzel+Decorative|Marcellus|Sanchez');
+        </style>
+          <Quiz data={ quizData } />
+        </div>
+        <div className="thing">
+          <Map google={this.props.google} zoom={14}>
 
-        <Marker onClick={this.onMarkerClick}
-          name={'Current location'} />
+          <Marker onClick={this.onMarkerClick}
+            name={'Current location'} />
 
-          <InfoWindow onClose={this.onInfoWindowClose}>
-          <div>
-        
+            <InfoWindow onClose={this.onInfoWindowClose}>
+
+            </InfoWindow>
+            </Map>
           </div>
-          </InfoWindow>
-          </Map>
       </div>
     );
   }
