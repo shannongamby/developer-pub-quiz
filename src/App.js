82cbaed19b505c0ac3d5f325ca40ecfe42066fd9
NaomiBid <naomi0bid@gmail.com>
2018-12-17 10:58:21 +0000

    tests passing, old bits of map code deleted

--- src/App.js
+++ src/App.js
@@ -3,10 +3,9 @@ import './App.css';
 import Quiz from './components/Quiz.js'
 import quizData from './quizquestions/data'
 import { Button } from 'react-bootstrap';
-import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
-import { GoogleComponent } from 'react-google-location'
 
-const API_KEY = "AIzaSyA2yE421Wa22_1aNakfIBWdQv265puS1EE"
+
+
 
 class App extends Component {
   constructor(props) {
