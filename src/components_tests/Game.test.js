97b9173a426808b8b2860e8fcb1cccef7b2222f2
Edyta Bednarz <edy.b.bednarz@gmail.com>
2018-12-20 18:08:51 +0000

    Added test to check first screen.

--- src/components_tests/Game.test.js
+++ src/components_tests/Game.test.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Game from '../components/Game';
+import quizData from '../quizquestions/data'
+
 import { shallow, mount, render } from 'enzyme';
 
 describe('<Game/>', () => {
@@ -8,5 +10,18 @@ describe('<Game/>', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Game />, div);
     ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('renders play for pints as the first view', () => {
+    const wrap = shallow( <Game data={ quizData } />)
+
+    expect(wrap.find('#play_button').prop('style')).not.toHaveProperty('display', 'none')
+    expect(wrap.find('#quiz_one').prop('style')).toHaveProperty('display', 'none')
+    expect(wrap.find('#switch_player').prop('style')).toHaveProperty('display', 'none')
+    expect(wrap.find('#quiz_two').prop('style')).toHaveProperty('display', 'none')
+    expect(wrap.find('#end_screen_1').prop('style')).toHaveProperty('display', 'none')
+    expect(wrap.find('#end_screen_2').prop('style')).toHaveProperty('display', 'none')
+    expect(wrap.find('#play_again').prop('style')).toHaveProperty('display', 'none')
+
   });
 });
