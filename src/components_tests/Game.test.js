6973bcf6167b9da55ca71bf8103798ca791a574c
Edyta Bednarz <edy.b.bednarz@gmail.com>
2018-12-20 19:09:39 +0000

    Added test to check map button.

--- src/components_tests/Game.test.js
+++ src/components_tests/Game.test.js
@@ -107,5 +107,27 @@ describe('<Game/>', () => {
       expect(wrap.find('#end_screen_2').prop('style')).not.toHaveProperty('display', 'none')
       expect(wrap.find('#play_again').prop('style')).not.toHaveProperty('display', 'none')
     });
-    
+
+    it('renders play again after second quiz is finished', () => {
+      const wrap = shallow( <Game data={ quizData } />)
+
+      wrap.find('#play_button').simulate('click')
+
+      wrap.find('#quiz_one Quiz').at(0).prop('handleQuizSwitch')()
+
+      wrap.find('#switch_player').simulate('click')
+
+      wrap.find('#quiz_two Quiz').at(0).prop('handleQuizSwitch')()
+
+      expect(wrap.find('#play_button').prop('style')).toHaveProperty('display', 'none')
+      expect(wrap.find('#quiz_one').prop('style')).toHaveProperty('display', 'none')
+      expect(wrap.find('#switch_player').prop('style')).toHaveProperty('display', 'none')
+      expect(wrap.find('#quiz_two').prop('style')).toHaveProperty('display', 'none')
+      expect(wrap.find('#end_screen_1').prop('style')).not.toHaveProperty('display', 'none')
+      expect(wrap.find('#end_screen_2').prop('style')).not.toHaveProperty('display', 'none')
+      expect(wrap.find('#play_again').prop('style')).not.toHaveProperty('display', 'none')
+      expect(wrap.find('#map_button').prop('style')).not.toHaveProperty('display', 'none')
+
+    });
+
 });
