4bd6cae8384b231a82414469652cf4fe55d5d041
Rupert Adams <RupertNCAdams@aol.com>
2018-12-20 12:21:43 +0000

    added custom icons for markers into a new img folder

--- src/components/Map.js
+++ src/components/Map.js
@@ -30,77 +30,187 @@ class Mapping extends Component {
   render(){
     return(
       <div className= "Map">
-      <Map style= {{ height: '70vh', width: '98.3%'}} google={this.props.google} zoom={14} initialCenter={{
+      <Map style= {{ height: '70vh', width: '98.3%'}} google={this.props.google} zoom={15} initialCenter={{
             lat: 51.517338,
             lng: -0.073279
           }}>
 
       <Marker onClick={this.onMarkerClick}
-        name={'Current location'} />
+        name={'Current location'}
+        icon={{
+          url: require('../img/Current-location-pin.png'),
+          scaledSize: { width: 45, height: 45},
+          anchor: { x: 15, y: 15 }
+        }}/>
 
       <Marker onClick={this.onMarkerClick}
         name={'The Culpepper'}
-        position={{lat: 51.5168721, lng: -0.0730934}} />
+        position={{lat: 51.5168721, lng: -0.0730934}}
+        icon={{
+          url: require('../img/beer-pin.png'),
+          scaledSize: { width: 30, height: 30},
+          anchor: { x: 15, y: 15 }
+        }}/>
       <Marker onClick={this.onMarkerClick}
         name={'Apples & Pears Bar'}
-        position={{lat: 51.5169396, lng: -0.070203}} />
+        position={{lat: 51.5169396, lng: -0.070203}}
+        icon={{
+          url: require('../img/beer-pin.png'),
+          scaledSize: { width: 30, height: 30},
+          anchor: { x: 15, y: 15 }
+        }} />
       <Marker onClick={this.onMarkerClick}
         name={'The Grocer'}
-        position={{lat: 51.5200787, lng: -0.0762836}} />
+        position={{lat: 51.5200787, lng: -0.0762836}}
+        icon={{
+          url: require('../img/beer-pin.png'),
+          scaledSize: { width: 30, height: 30},
+          anchor: { x: 15, y: 15 }
+        }} />
       <Marker onClick={this.onMarkerClick}
         name={'The Astronomer'}
-        position={{lat: 51.5179399, lng: -0.0788903}} />
+        position={{lat: 51.5179399, lng: -0.0788903}}
+        icon={{
+          url: require('../img/beer-pin.png'),
+          scaledSize: { width: 30, height: 30},
+          anchor: { x: 15, y: 15 }
+        }} />
       <Marker onClick={this.onMarkerClick}
         name={'The Magpie'}
-        position={{lat: 51.5171484, lng: -0.0791323}} />
+        position={{lat: 51.5171484, lng: -0.0791323}}
+        icon={{
+          url: require('../img/beer-pin.png'),
+          scaledSize: { width: 30, height: 30},
+          anchor: { x: 15, y: 15 }
+        }} />
       <Marker onClick={this.onMarkerClick}
         name={'Leman Street Tavern'}
-        position={{lat: 51.5138466, lng: -0.0705016}} />
+        position={{lat: 51.5138466, lng: -0.0705016}}
+        icon={{
+          url: require('../img/beer-pin.png'),
+          scaledSize: { width: 30, height: 30},
+          anchor: { x: 15, y: 15 }
+        }} />
       <Marker onClick={this.onMarkerClick}
         name={"Goodman's Field"}
-        position={{lat: 51.5114809, lng: -0.0728171}} />
+        position={{lat: 51.5114809, lng: -0.0728171}}
+        icon={{
+          url: require('../img/beer-pin.png'),
+          scaledSize: { width: 30, height: 30},
+          anchor: { x: 15, y: 15 }
+        }} />
       <Marker onClick={this.onMarkerClick}
         name={"The Ten Bells"}
-        position={{lat: 51.519358, lng: -0.0743816}} />
+        position={{lat: 51.519358, lng: -0.0743816}}
+        icon={{
+          url: require('../img/beer-pin.png'),
+          scaledSize: { width: 30, height: 30},
+          anchor: { x: 15, y: 15 }
+        }} />
       <Marker onClick={this.onMarkerClick}
         name={"The Pride of Spitalfields"}
-        position={{lat: 51.5189067, lng: -0.0712574}} />
+        position={{lat: 51.5189067, lng: -0.0712574}}
+        icon={{
+          url: require('../img/beer-pin.png'),
+          scaledSize: { width: 30, height: 30},
+          anchor: { x: 15, y: 15 }
+        }} />
       <Marker onClick={this.onMarkerClick}
         name={"The Golden Hart"}
-        position={{lat: 51.5201174, lng: -0.0742873}} />
+        position={{lat: 51.5201174, lng: -0.0742873}}
+        icon={{
+          url: require('../img/beer-pin.png'),
+          scaledSize: { width: 30, height: 30},
+          anchor: { x: 15, y: 15 }
+        }} />
       <Marker onClick={this.onMarkerClick}
         name={"The Diner Spitalfields"}
-        position={{lat: 51.51948, lng: -0.075858}} />
+        position={{lat: 51.51948, lng: -0.075858}}
+        icon={{
+          url: require('../img/beer-pin.png'),
+          scaledSize: { width: 30, height: 30},
+          anchor: { x: 15, y: 15 }
+        }} />
       <Marker onClick={this.onMarkerClick}
         name={"The Williams Ale & Cider House"}
-        position={{lat: 51.5183664, lng: -0.0784902}} />
+        position={{lat: 51.5183664, lng: -0.0784902}}
+        icon={{
+          url: require('../img/beer-pin.png'),
+          scaledSize: { width: 30, height: 30},
+          anchor: { x: 15, y: 15 }
+        }} />
       <Marker onClick={this.onMarkerClick}
         name={"The Bell"}
-        position={{lat: 51.5158898, lng: -0.0757336}} />
+        position={{lat: 51.5158898, lng: -0.0757336}}
+        icon={{
+          url: require('../img/beer-pin.png'),
+          scaledSize: { width: 30, height: 30},
+          anchor: { x: 15, y: 15 }
+        }} />
       <Marker onClick={this.onMarkerClick}
         name={"The Hoop and Grapes"}
-        position={{lat: 51.5142071, lng: -0.0741732}} />
+        position={{lat: 51.5142071, lng: -0.0741732}}
+        icon={{
+          url: require('../img/beer-pin.png'),
+          scaledSize: { width: 30, height: 30},
+          anchor: { x: 15, y: 15 }
+        }} />
       <Marker onClick={this.onMarkerClick}
         name={"Alice"}
-        position={{lat: 51.5150812, lng: -0.0781154}} />
+        position={{lat: 51.5150812, lng: -0.0781154}}
+        icon={{
+          url: require('../img/beer-pin.png'),
+          scaledSize: { width: 30, height: 30},
+          anchor: { x: 15, y: 15 }
+        }} />
       <Marker onClick={this.onMarkerClick}
         name={"The Bull"}
-        position={{lat: 51.5169543, lng: -0.0804983}} />
+        position={{lat: 51.5169543, lng: -0.0804983}}
+        icon={{
+          url: require('../img/beer-pin.png'),
+          scaledSize: { width: 30, height: 30},
+          anchor: { x: 15, y: 15 }
+        }} />
       <Marker onClick={this.onMarkerClick}
         name={"Grapeshots"}
-        position={{lat: 51.5180593, lng: -0.0775255}} />
+        position={{lat: 51.5180593, lng: -0.0775255}}
+        icon={{
+          url: require('../img/beer-pin.png'),
+          scaledSize: { width: 30, height: 30},
+          anchor: { x: 15, y: 15 }
+        }} />
       <Marker onClick={this.onMarkerClick}
         name={"Kings Stores"}
-        position={{lat: 51.5179809, lng: -0.0778893}} />
+        position={{lat: 51.5179809, lng: -0.0778893}}
+        icon={{
+          url: require('../img/beer-pin.png'),
+          scaledSize: { width: 30, height: 30},
+          anchor: { x: 15, y: 15 }
+        }} />
       <Marker onClick={this.onMarkerClick}
         name={"Dirty Dicks"}
-        position={{lat: 51.5180407, lng: -0.0796184}} />
+        position={{lat: 51.5180407, lng: -0.0796184}}
+        icon={{
+          url: require('../img/beer-pin.png'),
+          scaledSize: { width: 30, height: 30},
+          anchor: { x: 15, y: 15 }
+        }} />
       <Marker onClick={this.onMarkerClick}
         name={"The Water Poet"}
-        position={{lat: 51.5210245, lng: -0.0779423}} />
+        position={{lat: 51.5210245, lng: -0.0779423}}
+        icon={{
+          url: require('../img/beer-pin.png'),
+          scaledSize: { width: 30, height: 30},
+          anchor: { x: 15, y: 15 }
+        }} />
       <Marker onClick={this.onMarkerClick}
         name={"The Crown and Shuttle"}
-        position={{lat: 51.5224531, lng: -0.078184}} />
+        position={{lat: 51.5224531, lng: -0.078184}}
+        icon={{
+          url: require('../img/beer-pin.png'),
+          scaledSize: { width: 30, height: 30},
+          anchor: { x: 15, y: 15 }
+        }} />
       <InfoWindow onClose={this.onInfoWindowClose}
         marker={this.state.activeMarker}
         visible={true}>
